5fdf1ebfcb4187368bb56a4fc8a7b07de22a25e2
JLW1808 <jacob.woolbright1234@gmail.com>
2025-02-15 17:41:00 -0600

    Create CreateUser.java

---
+++ src/main/java/tech/touchgrass/endpoints/api/CreateUser.java
@@ -0,0 +1,59 @@
+package tech.touchgrass.endpoints.api;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.touchgrass.db.classes.user.User;
+import tech.touchgrass.db.classes.user.UserDatabase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class CreateUser implements HttpHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateUser.class);
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if ("POST".equals(exchange.getRequestMethod())) {
+            try (InputStream is = exchange.getRequestBody()) {
+                String requestBody = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                JSONObject json = new JSONObject(requestBody);
+
+                String email = json.getString("email");
+                String username = json.getString("username");
+                String password = json.getString("password");
+
+                if (UserDatabase.userExists(email, username)) {
+                    sendResponse(exchange, 409, "User already exists");
+                } else {
+                    User user = new User(username, email, password);
+                    boolean userCreated = UserDatabase.createUser(user);
+                    if (userCreated) {
+                        sendResponse(exchange, 201, "User created successfully");
+                    } else {
+                        sendResponse(exchange, 500, "Failed to create user");
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("Error processing request", e);
+                sendResponse(exchange, 500, "Internal server error");
+            }
+        } else {
+            sendResponse(exchange, 405, "Method not allowed");
+        }
+    }
+
+    private void sendResponse(HttpExchange exchange, int statusCode, String message) throws IOException {
+        byte[] responseBytes = message.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json; charset=UTF-8");
+        exchange.sendResponseHeaders(statusCode, responseBytes.length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(responseBytes);
+        }
+    }
+}
